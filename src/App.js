02f472bcb098536e51d4a86dbcb5d7a89e31c81a
Nahid Norouzi <106868573+nefbao@users.noreply.github.com>
2022-07-30 07:40:05 +0430

    created the structure

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer><a href='#'>open-source code</a> by Nahid Norouzi</footer>
+    </div>
+  );
+}
