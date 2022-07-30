8eabaceff8acb8f158b0150057197172412570c0
Nahid Norouzi <106868573+nefbao@users.noreply.github.com>
2022-07-30 07:41:08 +0430

    added link to git

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer><a href='#'>open-source code</a> by Nahid Norouzi</footer>
+      <footer><a href='https://github.com/nefbao/react-weather-project'>open-source code</a> by Nahid Norouzi</footer>
     </div>
   );
 }
