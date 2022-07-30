3526a3c1c27dc41407e0be96f8c2fca59e0d6fe3
Nahid Norouzi <106868573+nefbao@users.noreply.github.com>
2022-07-30 08:00:59 +0430

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
       <footer><a href='https://github.com/nefbao/react-weather-project'>open-source code</a> by Nahid Norouzi</footer>
+      </div>
     </div>
   );
 }
